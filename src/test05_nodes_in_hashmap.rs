0bb0e6bd9a87617d1346f9b3fb72eaae6645dd7d
u1roh <terada.yuichiro+github@gmail.com>
2020-01-12 22:20:33 +0900

    refactoring by sentinel

--- src/test05_nodes_in_hashmap.rs
+++ src/test05_nodes_in_hashmap.rs
@@ -45,64 +45,93 @@ fn test_pool() {
 
 mod list {
     use super::pool::Pool;
+    use std::ops::{Deref, DerefMut};
 
     pub struct Node<T> {
-        pub value: T,
+        value: Option<T>,
         next: *mut Self,
+        prev: *mut Self,
     }
     impl<T> Node<T> {
+        fn to_option(&self) -> Option<&Self> {
+            if self.value.is_some() {
+                Some(self)
+            } else {
+                None
+            }
+        }
         pub fn next(&self) -> Option<&Self> {
-            unsafe { std::mem::transmute(self.next) }
+            Self::to_option(unsafe { std::mem::transmute(self.next) })
+        }
+        pub fn prev(&self) -> Option<&Self> {
+            Self::to_option(unsafe { std::mem::transmute(self.prev) })
+        }
+    }
+    impl<T> Deref for Node<T> {
+        type Target = T;
+        fn deref(&self) -> &Self::Target {
+            self.value.as_ref().unwrap()
+        }
+    }
+    impl<T> DerefMut for Node<T> {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            self.value.as_mut().unwrap()
         }
     }
 
     pub struct List<T> {
         nodes: Pool<Node<T>>,
-        head: *mut Node<T>,
-        tail: *mut Node<T>,
+        sentinel: Box<Node<T>>,
     }
     impl<T> List<T> {
         pub fn new() -> Self {
+            let mut sentinel = Box::new(Node::<T> {
+                value: None,
+                next: std::ptr::null_mut(),
+                prev: std::ptr::null_mut(),
+            });
+            sentinel.next = sentinel.deref_mut() as *mut Node<T>;
+            sentinel.prev = sentinel.deref_mut() as *mut Node<T>;
             Self {
                 nodes: Pool::new(),
-                head: std::ptr::null_mut(),
-                tail: std::ptr::null_mut(),
+                sentinel,
             }
         }
         pub fn head(&self) -> Option<&Node<T>> {
-            unsafe { std::mem::transmute(self.head) }
+            Node::to_option(unsafe { std::mem::transmute(self.sentinel.next) })
         }
         pub fn tail(&self) -> Option<&Node<T>> {
-            unsafe { std::mem::transmute(self.tail) }
+            Node::to_option(unsafe { std::mem::transmute(self.sentinel.prev) })
         }
         pub fn is_empty(&self) -> bool {
-            self.head.is_null()
+            self.head().is_none()
         }
-        pub fn push_back(&mut self, value: T) {
-            let ptr = self.nodes.alloc(Node {
-                value,
-                next: std::ptr::null_mut(),
+        unsafe fn insert_unsafe(&mut self, next: *mut Node<T>, value: T) {
+            let prev: *mut Node<T> = (*next).prev;
+            let node: *mut Node<T> = self.nodes.alloc(Node {
+                value: Some(value),
+                next,
+                prev,
             }) as *mut Node<T>;
-            if self.is_empty() {
-                self.head = ptr;
-                self.tail = ptr;
-            } else {
-                unsafe { (*self.tail).next = ptr };
-                self.tail = ptr;
-            }
+            (*next).prev = node;
+            (*prev).next = node;
         }
         pub fn insert(&mut self, pos: *const Node<T>, value: T) -> bool {
-            if let Some(node) = self.nodes.get_mut(pos).map(|node| node as *mut Node<T>) {
-                unsafe {
-                    (*node).next = self.nodes.alloc(Node {
-                        value,
-                        next: (*node).next,
-                    }) as *mut Node<T>;
-                }
+            if let Some(next) = self.nodes.get_mut(pos) {
+                let next = next as *mut Node<T>;
+                unsafe { self.insert_unsafe(next, value) }
                 true
             } else {
                 false
             }
+        }
+        pub fn push_back(&mut self, value: T) {
+            let next = self.sentinel.deref_mut() as *mut Node<T>;
+            unsafe { self.insert_unsafe(next, value) }
+        }
+        pub fn push_front(&mut self, value: T) {
+            let next = self.sentinel.next;
+            unsafe { self.insert_unsafe(next, value) }
         }
     }
 }
@@ -125,7 +154,7 @@ impl<T> List<T> {
 }
 
 #[test]
-fn test() {
+fn test_list() {
     let mut list: List<usize> = List::new();
     assert!(list.head().is_none());
     assert!(list.tail().is_none());
@@ -134,17 +163,27 @@ fn test() {
     list.push_back(1);
     assert!(list.head().is_some());
     assert!(list.tail().is_some());
-    assert_eq!(list.head_f().value, 1);
-    assert_eq!(list.tail_f().value, 1);
+    assert_eq!(**list.head_f(), 1);
+    assert_eq!(**list.tail_f(), 1);
     assert!(list.head_f().next().is_none());
+    assert!(list.head_f().prev().is_none());
 
     list.push_back(2);
-    assert_eq!(list.head_f().value, 1);
-    assert_eq!(list.tail_f().value, 2);
-    assert_eq!(list.head_f().next_f().value, 2);
+    assert_eq!(**list.head_f(), 1);
+    assert_eq!(**list.tail_f(), 2);
+    assert_eq!(**list.head_f().next_f(), 2);
 
-    assert!(list.insert(list.head_f() as *const Node<usize>, 3));
-    assert_eq!(list.head_f().value, 1);
-    assert_eq!(list.head_f().next_f().value, 3);
-    assert_eq!(list.head_f().next_f().next_f().value, 2);
+    list.push_front(3);
+    assert_eq!(**list.head_f(), 3);
+    assert_eq!(**list.head_f().next_f(), 1);
+
+    assert!(list.insert(list.head_f().next_f() as *const _, 4));
+    assert_eq!(**list.head_f(), 3);
+    assert_eq!(**list.head_f().next_f(), 4);
+    assert_eq!(**list.head_f().next_f().next_f(), 1);
+    assert_eq!(**list.head_f().next_f().next_f().next_f(), 2);
+
+    let head = list.head_f();
+    //list.push_front(5);
+    println!("{}", **head);
 }
